818e1ed7e92f4120773b19a65fa2fc943e0ef3b1
kirin <ywl_allen@163.com>
2017-12-25 13:13:20 +0800

    按需加载echarts模块

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import echarts from 'echarts';
+// 引入 ECharts 主模块
+var echarts = require('echarts/lib/echarts');
+// 引入柱状图
+require('echarts/lib/chart/bar');
+// 引入提示框和标题组件
+require('echarts/lib/component/tooltip');
+require('echarts/lib/component/title');
 
 // 基于准备好的dom，初始化echarts实例
 var myChart = echarts.init(document.getElementById('main'));
