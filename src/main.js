59c78b864c95cf69939d96fca6ee0e2880ea0c0f
kirin <ywl_allen@163.com>
2017-12-25 14:47:20 +0800

    饼图与柱状图

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 var echarts = require('echarts/lib/echarts');
 // 引入柱状图
 require('echarts/lib/chart/bar');
+require('echarts/lib/chart/pie');
+
+require("echarts/lib/component/legendScroll");
+
 // 引入提示框和标题组件
 require('echarts/lib/component/tooltip');
 require('echarts/lib/component/title');
@@ -17,10 +21,33 @@ myChart.setOption({
     xAxis: {
         data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
     },
+    legend: {
+        data: ['销量', '访问来源']
+    },
+    toolbox: {
+        show: true,
+        feature: {
+            dataView: {readOnly: false},
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    roseType: 'angle',
     yAxis: {},
     series: [{
         name: '销量',
         type: 'bar',
         data: [5, 20, 36, 10, 10, 20]
+    }, {
+        name: '访问来源',
+        type: 'pie',
+        radius: '55%',
+        data: [
+            { value: 235, name: '视频广告' },
+            { value: 274, name: '联盟广告' },
+            { value: 310, name: '邮件营销' },
+            { value: 335, name: '直接访问' },
+            { value: 400, name: '搜索引擎' }
+        ]
     }]
 });
